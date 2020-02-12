b572cc889fb1f26703b40073c5e0e31745ef94b4
Gabriel Chouinard Létourneau <chouinardletourneaug@gmail.com>
2020-02-12 17:41:45 -0500

    first commit movieList app

---
+++ projetRecherche-movieListApp/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style/style.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './containers/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+const ApiKey = '448689459dc76da6251624edaee01113';
+
+ReactDOM.render(
+    <Provider
+        store={createStoreWithMiddleware(
+            reducers,
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        )}
+    >
+        <App />
+    </Provider>,
+    document.querySelector('.container')
+);
